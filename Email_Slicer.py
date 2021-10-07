84a85edbf757d640067d36a980ee8d76fad25786
swapno5896 <swapno5896@yahoo.com>
2021-10-07 19:08:13 +0600

    emile sliceing is done

--- Email_Slicer.py
+++ Email_Slicer.py
@@ -2,3 +2,5 @@ user_input = input('Enter Email Adderss : ').strip()
 user_name = user_input[:user_input.find('@')]
 domain_name = user_input[user_input.find('@')+1:]
 print(f"user name is {user_name} and domain name is {domain_name}")
+
+
